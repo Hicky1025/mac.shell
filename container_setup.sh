a6ea2c2425ef665b5ccfdb4d00445d56fc2085b3
Hicky1025 <plane1025.ict@gmail.com>
2021-04-13 17:04:43 +0900

    master

--- container_setup.sh
+++ container_setup.sh
@@ -3,4 +3,4 @@
 read -e -p "plz insert path : " container_path
 mkdir -p $container_path
 cp /Users/kouki/devs/container/container.* $container_path
-touch docker-copose.yml
+touch $container_path/docker-copose.yml
